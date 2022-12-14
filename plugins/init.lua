7cd2665d2ce23a9cb027ad7df9503e03c4507879
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2022-12-13 22:53:49 -0600

    Integration tmux

--- plugins/init.lua
+++ plugins/init.lua
@@ -45,6 +45,21 @@ return {
     end,
   },
   ["jay-babu/mason-null-ls.nvim"] = {},
+  ["alexghergh/nvim-tmux-navigation"] = {
+    config = function()
+      require("nvim-tmux-navigation").setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      }
+    end,
+  },
 }
 
 -- load it after nvim-lspconfig cuz we lazy loaded lspconfig}
